219fc34e2849bf7a88b44957ae9687e109f4b6c9
Sylvain <squoilin@uliege.be>
2021-11-16 12:39:13 +0100

    updating deprecated functions

--- El_Espino_Analisis/PV_Curtailment_Scenarios.py
+++ El_Espino_Analisis/PV_Curtailment_Scenarios.py
@@ -9,28 +9,28 @@ Created on Mon Jun 14 15:51:51 2021
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
+#import matplotlib.patches as mpatches
 import matplotlib.lines as mlines
-import matplotlib.ticker as mtick
+#import matplotlib.ticker as mtick
 import matplotlib.pylab as pylab
-from scipy.stats import pearsonr
-from mpl_toolkits.mplot3d import Axes3D
+#from scipy.stats import pearsonr
+#from mpl_toolkits.mplot3d import Axes3D
 from sklearn import linear_model
-from sklearn.metrics import r2_score
-import statsmodels.api as sm
+#from sklearn.metrics import r2_score
+#import statsmodels.api as sm
 from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C, Matern, ExpSineSquared, RationalQuadratic 
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.ensemble import RandomForestRegressor
-from scipy.stats import randint as sp_randint
-from sklearn.model_selection import RandomizedSearchCV
-from sklearn.model_selection import GridSearchCV
+#from scipy.stats import randint as sp_randint
+#from sklearn.model_selection import RandomizedSearchCV
+#from sklearn.model_selection import GridSearchCV
 
 #%%
 
 # load data
 
 data = pd.read_csv('Data/Data_Espino_Thesis_Fill_2.csv', header=0,index_col=0)
-index = pd.DatetimeIndex(start='2016-01-01 00:00:00', periods=166464, freq=('5min'))
+index = pd.date_range(start='2016-01-01 00:00:00', periods=166464, freq=('5min'))
 data.index = index
 Area = 1.61*0.946*240 # 1.61*0.946*240 # 1.65*0.99*240
 
@@ -226,7 +226,7 @@ data_hourly_describe = data_hourly.describe()
 #%%
 Data_2 = pd.read_csv('Results/Gutierrez_Data.csv', index_col=0)            
 
-index2 = pd.DatetimeIndex(start='2013-01-01 01:00:00', periods=8760, 
+index2 = pd.date_range(start='2013-01-01 01:00:00', periods=8760, 
                                    freq=('H'))
 
 start = index2.get_loc('2013-03-21 01:00:00')
@@ -239,7 +239,7 @@ Gut_Data_2  = Data_2[:end]
 
 Gut_Data =  Gut_Data_1.append(Gut_Data_2) 
 
-index_3 = pd.DatetimeIndex(start='2016-03-21 01:00:00', periods=8760, 
+index_3 = pd.date_range(start='2016-03-21 01:00:00', periods=8760, 
                                    freq=('H'))
 
 Gut_Data.index = index_3
